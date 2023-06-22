0fc4ba80d282bceeb70039a01bbd187fed1a51e9
即兴小索奇 <91357188+impsuoqi@users.noreply.github.com>
2023-06-23 00:26:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//toLocaleString():将当前日期和时间转换为本地格式的字符串，这个字符串会被设置为span元素的innerHTML属性，从而在页面中显示当前时间。
+// toLocaleString():将当前日期和时间转换为本地格式的字符串，这个字符串会被设置为span元素的innerHTML属性，从而在页面中显示当前时间。
 // 引入js写入在head中，确保页面加载完毕，window.onload包裹
 window.onload = function() {
     var datetime = document.getElementById('datetime');
     setInterval(function() {
       var now = new Date();
-      datetime.innerHTML = now.toLocaleString();
+      var options = { weekday: 'long' }; // 星期几选项
+      var weekday = now.toLocaleString('zh-CN', options); // 获取当前日期的星期几
+      datetime.innerHTML =now.toLocaleString() +" " + weekday; // 将星期几设置为元素的内容
     }, 1000);
   };
   
